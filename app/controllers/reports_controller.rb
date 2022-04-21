e3c9917cc53b16c682b19232a5dab56f3d21189b
Arseniy Kuzmenkov <ars20022002@mail.ru>
2022-04-21 15:19:15 +0300

    Added forgoten block

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,12 +6,17 @@ class ReportsController < ApplicationController
   # curl -d '{"reptype":"expensive", "depth":10}' -H "Content-Type: application/json" -X POST http://localhost:3000/reports
   # Creates a new report record with required "depth" and "reptype"
   def create
-    report = Report.create(report_params)
+    report = Report.new(report_params)
 
-    # Starts an async task of updating "result" field with report data from hw1
-    ReportUpdaterJob.perform_async(report.id)
+    if report.save
+      # Starts an async task of updating "result" field with report data from hw1
+      ReportUpdaterJob.perform_async(report.id)
 
-    render json: { result: 'true' }
+      render json: { result: 'true' }
+    else
+      render json: { result: 'false', error: report.errors[:depth] }
+    end
+
   rescue ArgumentError => e
     render json: { result: 'false', error: e.message }
   end
@@ -22,7 +27,7 @@ class ReportsController < ApplicationController
   end
 
   private
-  
+
   def report_params
     params.require(:report).permit(:reptype, :depth)
   end
